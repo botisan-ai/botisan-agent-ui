cffcedbec80915941b5e807f603a6f94e3ef8ba0
Simon Liang <simon@x-tech.io>
2023-08-10 17:33:12 +0800

    temporarily disables auth for now

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { type DefaultSession } from 'next-auth'
 import GitHub from 'next-auth/providers/github'
+import CredentialsProvider from "next-auth/providers/credentials";
 
 declare module 'next-auth' {
   interface Session {
@@ -15,7 +16,27 @@ export const {
   auth,
   CSRF_experimental // will be removed in future
 } = NextAuth({
-  providers: [GitHub],
+  providers: [CredentialsProvider({
+    name: 'Credentials',
+    credentials: {
+      username: { label: "Username", type: "text" },
+      password: {  label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      // Add logic here to look up the user from the credentials supplied
+      const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+      if (user) {
+        // Any object returned will be saved in `user` property of the JWT
+        return user
+      } else {
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      }
+    }
+  })],
   callbacks: {
     jwt({ token, profile }) {
       if (profile) {
@@ -28,7 +49,7 @@ export const {
       return !!auth?.user // this ensures there is a logged in user for -every- request
     }
   },
-  pages: {
-    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
-  }
+  // pages: {
+  //   signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
+  // }
 })
