7a34ecf45eed989b61a6777611468ab59803dbc1
Simon Liang <simon@x-tech.io>
2023-08-26 14:40:55 +0800

    using monaco instead

--- components/editor/chat-editor-message-edit.tsx
+++ components/editor/chat-editor-message-edit.tsx
@@ -1,49 +1,89 @@
 // Inspired by Chatbot-UI and modified to fit the needs of this project
 // @see https://github.com/mckaywrigley/chatbot-ui/blob/main/components/Chat/ChatMessage.tsx
+'use client'
 
 import { useState } from 'react'
 
-import ReactJson from 'react-json-view'
+import Editor, { EditorProps, type Monaco } from '@monaco-editor/react'
 
 import { Message } from '@/lib/types'
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectItem } from '@/components/ui/select'
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectGroup,
+  SelectItem
+} from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Switch } from '@/components/ui/switch'
 
 export interface ChatEditorMessageProps {
-  message: Message;
-  setIsEditing: (param: any) => void;
-  saveMessage: (param: Message) => void;
-  cancelInsert: () => void;
+  message: Message
+  setIsEditing: (param: any) => void
+  saveMessage: (param: Message) => void
+  cancelInsert: () => void
 }
 
 export function ChatEditorMessageEdit({
   message,
   setIsEditing,
   saveMessage,
-  cancelInsert,
+  cancelInsert
 }: ChatEditorMessageProps) {
-  const [editRole, setEditRole] = useState<any>(message.role);
-  const [editName, setEditName] = useState(message.name);
-  const [editContent, setEditContent] = useState(message.content);
-  const [editFunctionContent, setEditFunctionContent] = useState(message.role === 'function' ? JSON.parse(message.content) : {})
-  const [editWithFunctionCall, setWithFunctionCall] = useState(!!message.function_call);
-  const [editFunctionCall, setEditFunctionCall] = useState(message.function_call ? {
-    name: message.function_call.name,
-    arguments: JSON.parse(message.function_call.arguments)
-  } : {name: '', arguments: {}});
+  const [editRole, setEditRole] = useState<any>(message.role)
+  const [editName, setEditName] = useState(message.name)
+  const [editContent, setEditContent] = useState(message.content)
+  const [editFunctionContent, setEditFunctionContent] = useState(
+    message.role === 'function' ? JSON.parse(message.content) : {}
+  )
+  const [editWithFunctionCall, setWithFunctionCall] = useState(
+    !!message.function_call
+  )
+  const [editFunctionCall, setEditFunctionCall] = useState(
+    message.function_call
+      ? {
+          name: message.function_call.name,
+          arguments: JSON.parse(message.function_call.arguments)
+        }
+      : { name: '', arguments: {} }
+  )
+
+  const editorOptions: EditorProps['options'] = {
+    minimap: {
+      enabled: false
+    },
+    fontSize: 14,
+    automaticLayout: true,
+    scrollBeyondLastLine: false,
+    scrollbar: {
+      alwaysConsumeMouseWheel: false
+    }
+  }
+
+  // function handleDidMount(editor: any, monaco: Monaco) {
+  //   monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+  //     validate: true,
+  //   });
+  // }
 
   function cancelEdit() {
-    setIsEditing(false);
-    setEditRole(message.role);
-    setEditName(message.name);
-    setEditContent(message.content);
-    setEditFunctionContent(message.role === 'function' ? JSON.parse(message.content) : {});
-    setEditFunctionCall(message.function_call ? message.function_call : {name: '', arguments: {}})
+    setIsEditing(false)
+    setEditRole(message.role)
+    setEditName(message.name)
+    setEditContent(message.content)
+    setEditFunctionContent(
+      message.role === 'function' ? JSON.parse(message.content) : {}
+    )
+    setEditFunctionCall(
+      message.function_call
+        ? message.function_call
+        : { name: '', arguments: {} }
+    )
     if (message.isEdit) {
-      cancelInsert();
+      cancelInsert()
     }
   }
 
@@ -51,20 +91,25 @@ export function ChatEditorMessageEdit({
     saveMessage({
       role: editRole,
       name: editName,
-      content: editRole === 'function' ? JSON.stringify(editFunctionContent) : editContent,
-      function_call: editWithFunctionCall ? {
-        name: editFunctionCall.name,
-        arguments: JSON.stringify(editFunctionCall.arguments)
-      } : undefined
-    });
-    setIsEditing(false);
+      content:
+        editRole === 'function'
+          ? JSON.stringify(editFunctionContent)
+          : editContent,
+      function_call: editWithFunctionCall
+        ? {
+            name: editFunctionCall.name,
+            arguments: JSON.stringify(editFunctionCall.arguments)
+          }
+        : undefined
+    })
+    setIsEditing(false)
   }
 
   return (
-    <div className='flex w-full flex-col pl-12'>
-      <div className='mb-5 flex w-full flex-col justify-between'>
-        <p className='mb-2'>Role:</p>
-        <Select value={editRole} onValueChange={(value) => setEditRole(value)}>
+    <div className="flex w-full flex-col pl-12">
+      <div className="mb-5 flex w-full flex-col justify-between">
+        <p className="mb-2">Role:</p>
+        <Select value={editRole} onValueChange={value => setEditRole(value)}>
           <SelectTrigger>
             <SelectValue placeholder="Select role" />
           </SelectTrigger>
@@ -79,60 +124,121 @@ export function ChatEditorMessageEdit({
         </Select>
       </div>
 
-      {
-        ['user', 'system'].includes(editRole) && (
-          <div className='mb-5'>
-            <Textarea value={editContent} onChange={(ev) => setEditContent(ev.target.value)} />
-          </div>
-        )
-      }
+      {['user', 'system'].includes(editRole) && (
+        <div className="mb-5">
+          <Textarea
+            value={editContent}
+            onChange={ev => setEditContent(ev.target.value)}
+          />
+        </div>
+      )}
 
-      {
-        editRole === 'assistant' && (
-          <div className='mb-5'>
-            <div className='mb-5 flex'>
-              <label htmlFor="function-call" style={{ paddingRight: 15 }}>
-                Function Call
-              </label>
-              <Switch checked={editWithFunctionCall} onCheckedChange={(value) => setWithFunctionCall(value)} id="function-call" />
+      {editRole === 'assistant' && (
+        <div className="mb-5">
+          <div className="mb-5 flex">
+            <label htmlFor="function-call" style={{ paddingRight: 15 }}>
+              Function Call
+            </label>
+            <Switch
+              checked={editWithFunctionCall}
+              onCheckedChange={value => setWithFunctionCall(value)}
+              id="function-call"
+            />
+          </div>
+
+          {editWithFunctionCall && (
+            <Editor
+              height="150px"
+              theme="vs-dark"
+              defaultLanguage="json"
+              options={editorOptions}
+              defaultValue={JSON.stringify(editFunctionCall, null, 2)}
+              onMount={(editor, monaco) => {
+                monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+                  validate: true,
+                  schemaValidation: 'error',
+                  schemas: [
+                    {
+                      uri: 'https://dummy.json.schema/function-call',
+                      fileMatch: ['*'],
+                      schema: {
+                        type: 'object',
+                        properties: {
+                          name: {
+                            type: 'string'
+                          },
+                          arguments: {
+                            type: 'object'
+                          }
+                        },
+                        required: ['name', 'arguments'],
+                      },
+                    }
+                  ],
+                })
+              }}
+              onChange={(value, ev) => setEditFunctionCall(JSON.parse(value!))}
+            />
+          )}
+
+          {!editWithFunctionCall && (
+            <Textarea
+              value={editContent}
+              onChange={ev => setEditContent(ev.target.value)}
+            />
+          )}
+        </div>
+      )}
+
+      {['function'].includes(editRole) && (
+        <div className="mb-5">
+          <div className="mb-5">
+            <div className="mb-5 flex w-full flex-col justify-between">
+              <p className="mb-2">Name:</p>
+              <Input
+                value={editName}
+                onChange={ev => setEditName(ev.target.value)}
+              />
             </div>
 
-            {
-              editWithFunctionCall && (
-                <ReactJson theme="monokai" src={editFunctionCall} onEdit={(value) => setEditFunctionCall(value.updated_src as any)} onAdd={(value) => setEditFunctionCall(value.updated_src as any)} />
-              )
-            }
-
-            {
-              !editWithFunctionCall && (
-                <Textarea value={editContent} onChange={(ev) => setEditContent(ev.target.value)} />
-              )
-            }
-          </div>
-        )
-      }
-
-      {
-        ['function'].includes(editRole) && (
-          <div className='mb-5'>
-            <div className='mb-5'>
-              <div className='mb-5 flex w-full flex-col justify-between'>
-                <p className='mb-2'>Name:</p>
-                <Input value={editName} onChange={(ev) => setEditName(ev.target.value)} />
-              </div>
-
-              <div className='py-3'>
-                <ReactJson theme="monokai" src={editFunctionContent} onEdit={(value) => setEditFunctionContent(value.updated_src)} onAdd={(value) => setEditFunctionContent(value.updated_src)} />
-              </div>
+            <div className="py-3">
+              <Editor
+                height="150px"
+                theme="vs-dark"
+                defaultLanguage="json"
+                options={editorOptions}
+                defaultValue={JSON.stringify(editFunctionContent, null, 2)}
+                onMount={(editor, monaco) => {
+                  monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+                    validate: true,
+                    schemaValidation: 'error',
+                    schemas: [
+                      {
+                        uri: 'https://dummy.json.schema/function-call-return',
+                        fileMatch: ['*'],
+                        schema: {
+                          type: 'object',
+                        },
+                      }
+                    ],
+                  })
+                }}
+                onChange={(value, ev) =>
+                  setEditFunctionContent(JSON.parse(value!))
+                }
+              />
             </div>
-
           </div>
-        )
-      }
+        </div>
+      )}
 
-      <div className='flex justify-center'>
-        <Button className='mr-5' onClick={saveEdit}>Save</Button>
-        <Button variant="destructive" onClick={cancelEdit}>Cancel</Button>
+      <div className="flex justify-center">
+        <Button className="mr-5" onClick={saveEdit}>
+          Save
+        </Button>
+        <Button variant="destructive" onClick={cancelEdit}>
+          Cancel
+        </Button>
       </div>
     </div>
   )
--- components/editor/chat-editor-message.tsx
+++ components/editor/chat-editor-message.tsx
@@ -1,7 +1,7 @@
 // Inspired by Chatbot-UI and modified to fit the needs of this project
 // @see https://github.com/mckaywrigley/chatbot-ui/blob/main/components/Chat/ChatMessage.tsx
 
-import { useState, Dispatch, SetStateAction } from 'react'
+import { useState } from 'react'
 
 import { Message } from '@/lib/types'
 import { cn } from '@/lib/utils'
@@ -9,10 +9,10 @@ import { ChatEditorMessageView } from '@/components/editor/chat-editor-message-v
 import { ChatEditorMessageEdit } from '@/components/editor/chat-editor-message-edit'
 
 export interface ChatEditorMessageProps {
-  message: Message;
-  setMessage: (param: Message) => void;
-  insertMessage: (type: string) => void;
-  cancelInsert: () => void;
+  message: Message
+  setMessage: (param: Message) => void
+  insertMessage: (type: string) => void
+  cancelInsert: () => void
 }
 
 export function ChatEditorMessage({
@@ -22,11 +22,15 @@ export function ChatEditorMessage({
   cancelInsert,
   ...props
 }: ChatEditorMessageProps) {
-  const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false)
 
   function saveMessage(value: Message) {
-    console.log(message);
-    setMessage(value);
+    if (value.role === 'assistant' && value.function_call) {
+      // @ts-ignore
+      delete value.content
+    }
+
+    setMessage(value)
   }
 
   return (
@@ -34,13 +38,20 @@ export function ChatEditorMessage({
       className={cn('group relative mb-4 flex items-start md:-ml-12')}
       {...props}
     >
-      {
-        message.isEdit || isEditing ? (
-          <ChatEditorMessageEdit message={message} saveMessage={saveMessage} setIsEditing={setIsEditing} cancelInsert={cancelInsert} />
-        ) : (
-          <ChatEditorMessageView message={message} setIsEditing={setIsEditing} insertMessage={insertMessage} />
-        )
-      }
+      {message.isEdit || isEditing ? (
+        <ChatEditorMessageEdit
+          message={message}
+          saveMessage={saveMessage}
+          setIsEditing={setIsEditing}
+          cancelInsert={cancelInsert}
+        />
+      ) : (
+        <ChatEditorMessageView
+          message={message}
+          setIsEditing={setIsEditing}
+          insertMessage={insertMessage}
+        />
+      )}
     </div>
   )
 }
